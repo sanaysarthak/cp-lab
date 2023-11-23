189989f1a4644faee818142317f4e801ab65965d
sanaysarthak <66907138+sanaysarthak@users.noreply.github.com>
2023-11-23 13:58:57 +0530

    Create func-type1.c

---
+++ 23-11-2023/func-type1.c
@@ -0,0 +1,16 @@
+/* Type 1:-
+Parameters Passed -> No
+Returns -> No */
+
+void sum()
+{
+    int a=10, b=20, c=0;
+    c= a+b;
+    printf("%d", c);
+}
+
+int main()
+{
+    sum();
+    return 0;
+}
